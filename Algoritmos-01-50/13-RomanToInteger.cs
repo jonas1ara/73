45df92dbe6962bc2db12091536b26eb738ca671e
Jonas Lara <adrianlr2515@outlook.com>
2022-12-28 23:41:44 -0600

    UPDATE 13 y 14

--- Algoritmos-01-50/13-RomanToInteger.cs
+++ Algoritmos-01-50/13-RomanToInteger.cs
@@ -4,9 +4,43 @@ namespace Leetcode
 {
     class Program
     {
+        static int Mapping(char ch)
+        {
+            switch (ch)
+            {
+                case 'I': return 1;
+                case 'V': return 5;
+                case 'X': return 10;
+                case 'L': return 50;
+                case 'C': return 100;
+                case 'D': return 500;
+                case 'M': return 1000;
+                default: return 0;
+            }
+        }
+
+        public static int RomanToInt(string s)
+        {
+            var result = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (i > 0 && Mapping(s[i]) > Mapping(s[i - 1]))
+                {
+                    result += Mapping(s[i]) - Mapping(s[i - 1]) * 2;
+                }
+                else
+                {
+                    result += Mapping(s[i]);
+                }
+            }
+
+            return result;
+        }
         public static void Main (string[] args)
         {
-            Console.WriteLine();
+            string s = "MCMXCIV";
+            Console.WriteLine("Input: " + s);
+            Console.WriteLine("Output: " + RomanToInt(s));
         }
     }
 }
