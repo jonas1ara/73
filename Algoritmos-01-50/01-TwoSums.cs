4967397fc3d0e20d5bcf4e62228dbe07f72059db
Jonas Lara <adrianlr2515@outlook.com>
2022-12-29 23:21:14 -0600

    Update 01

--- Algoritmos-01-50/01-TwoSums.cs
+++ Algoritmos-01-50/01-TwoSums.cs
@@ -1,37 +1,27 @@
 using System;
 using System.Collections.Generic;
 
-namespace LeetCode
+static int[] TwoSum(int[] nums, int target)
 {
-    class Program
+    var dic = new Dictionary<int, int>();
+
+    for (int i = 0; i < nums.Length; i++)
     {
-        public static int[] TwoSum(int[] nums, int target)
-        {
-            var dic = new Dictionary<int, int>();
+		int t = target - nums[i];
 
-            for (int i = 0; i < nums.Length; i++)
-            {
-				int t = target - nums[i];
-
-                if (dic.ContainsKey(t)) return new int[] { dic[t], i };
+        if (dic.ContainsKey(t)) return new int[] { dic[t], i };
     
-                dic[nums[i]] = i;
-            }
-
-            return new int[] { };
-        }
-    
-	    public static void Main (string[] args)
-        {
-            int[] nums = { 2, 1, 5, 3 };
-            int target = 4;
-	
-			Console.WriteLine("Input: nums = [" + String.Join(", ", nums) + "], target = " + target);
-
-    		int[] result = TwoSum(nums, target);	
-			Console.WriteLine("Output: [" + String.Join(", ",result) + "]");
-	
-
-        }
+        dic[nums[i]] = i;
     }
+    return new int[] { };
 }
+    
+int[] nums = { 2, 1, 5, 3 };
+int target = 4;
+//Print the input
+Console.WriteLine("Input: nums = [" + String.Join(", ", nums) + "], target = " + target);
+
+int[] result = TwoSum(nums, target);	
+
+//Print the output
+Console.WriteLine("Output: [" + String.Join(", ",result) + "]");
