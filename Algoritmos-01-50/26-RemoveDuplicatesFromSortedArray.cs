d3ff8aa1742686e225854230a63f5732f4f593e8
Jonas Lara <adrianlr2515@outlook.com>
2022-12-30 21:33:15 -0600

    UPDATE

--- Algoritmos-01-50/26-RemoveDuplicatesFromSortedArray.cs
+++ Algoritmos-01-50/26-RemoveDuplicatesFromSortedArray.cs
@@ -1,12 +1,24 @@
 using System;
+using System.Collections.Generic;
 
-namespace LeetCode
+int RemoveDuplicates(int[] nums)
 {
-    class Program
+    if (nums.Length <= 1) { return nums.Length; }
+
+    var index = 0;
+    var lastValue = nums[index];
+    for (int i = 0; i < nums.Length; i++)
     {
-        public static void Main (string[] args)
-        {
-            Console.WriteLine();
-        }
+        if (nums[i] == lastValue) { continue; }
+
+        nums[++index] = nums[i];
+        lastValue = nums[index];
     }
+
+    return index + 1;
 }
+
+int[] nums = {1, 1, 2};
+Console.WriteLine("Input: [" + string.Join(", ", nums) + "]");
+int length = RemoveDuplicates(nums);
+Console.WriteLine("Output: " + length);
