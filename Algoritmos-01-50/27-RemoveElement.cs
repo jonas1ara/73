be63cee6b0b5b4f434f6f6671e79b3058bfe59b0
Jonas Lara <adrianlr2515@outlook.com>
2022-12-30 21:40:24 -0600

    UPDATE

--- Algoritmos-01-50/27-RemoveElement.cs
+++ Algoritmos-01-50/27-RemoveElement.cs
@@ -1,29 +1,20 @@
 using System;
+using System.Collections.Generic;
 
-namespace LeetCode
+int RemoveElement(int[] nums, int val)
 {
-    class Program
+    var lastIndex = nums.Length - 1;
+
+    for (int i = 0; i <= lastIndex; i++)
     {
-        public static int RemoveElement(int[] nums, int val)
-        {
-            var lastIndex = nums.Length - 1;
-
-            for (int i = 0; i <= lastIndex; i++)
-            {
-                if (nums[i] == val)
-                {
-                    nums[i--] = nums[lastIndex--];
-                }
-            }
-
-            return lastIndex + 1;
-        }
-        public static void Main (string[] args)
-        {
-            var nums = new int[] { 3, 2, 2, 3 };
-            var val = 3;
-            var Result = RemoveElement(nums, val);
-            Console.WriteLine(Result);
-        }
+        if (nums[i] == val)
+            nums[i--] = nums[lastIndex--];
     }
+    return lastIndex + 1;
 }
+        
+int[] nums = {3, 2, 2, 3};
+int val = 3;
+Console.WriteLine("Input: nums = [" + string.Join(", ", nums) + "], val = " + val);
+int length = RemoveElement(nums, val);
+Console.WriteLine("Output: " + length);
