8c77e7fd572ead76628a07f0dbb19569372711c4
Jonas Lara <adrianlr2515@outlook.com>
2023-01-05 09:53:23 -0600

    01 - 20

--- 017.-LetterCombinationsOfAPhoneNumber/Program.cs
+++ 017.-LetterCombinationsOfAPhoneNumber/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+IList<string> LetterCombinations(string digits)
+{
+    string[] phoneChars = new string[] { " ",
+                                                 "",
+                                                 "abc",
+                                                 "def",
+                                                 "ghi",
+                                                 "jkl",
+                                                 "mno",
+                                                 "pqrs",
+                                                 "tuv",
+                                                 "wxyz"
+                                               };
+
+    if (digits.Length == 0) return new List<string>();
+
+    var results = new List<string>() { "" };
+    foreach (var digit in digits)
+    {
+        var keys = phoneChars[digit - '0'];
+        if (keys.Length == 0) continue;
+
+        var temp = new List<string>();
+        foreach (var result in results)
+            foreach (var ch in keys)
+                temp.Add(result + ch.ToString());
+
+        results = temp;
+    }
+
+    if (results.Count == 1 && results[0] == "") results.Clear();
+    return results;
+}
