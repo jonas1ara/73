eab5c73ed3041be8fc8a8783fb51b0c9d7e255b9
Jonas Lara <adrianlr2515@outlook.com>
2023-01-05 09:59:30 -0600

    20 - 25

---
+++ 023.-MergeKSortedLists/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+public ListNode MergeKLists(ListNode[] lists)
+{
+    if (lists == null || lists.Length == 0) return null;
+
+    return MergeKHelper(lists, 0, lists.Length - 1);
+}
+
+public ListNode MergeKHelper(ListNode[] lists, int i, int j)
+{
+    if (i == j) return lists[i];
+
+    int mid = i + (j - i) / 2;
+    var left = MergeKHelper(lists, i, mid);
+    var right = MergeKHelper(lists, mid + 1, j);
+    return MergeTwoLists(left, right);
+}
+
+private ListNode MergeTwoLists(ListNode l1, ListNode l2)
+{
+    var head = new ListNode(-1);
+    var current = head;
+
+    while (l1 != null && l2 != null)
+    {
+        if (l1.val <= l2.val)
+        {
+            current.next = l1;
+            l1 = l1.next;
+        }
+        else
+        {
+            current.next = l2;
+            l2 = l2.next;
+        }
+        current = current.next;
+    }
+
+    current.next = l1 == null ? l2 : l1;
+    return head.next;
+}
