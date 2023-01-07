b6358a181881c81a51d0f7ea2c4b1714d136a42e
Jonas Lara <adrianlr2515@outlook.com>
2023-01-06 23:04:41 -0600

    09

--- 009.-PalindromeNumber/Program.cs
+++ 009.-PalindromeNumber/Program.cs
@@ -1,34 +1,24 @@
 ﻿using System;
 
-namespace LeetCode
+bool IsPalindrome(int x)
 {
-    class Program
+    if (x < 0) { return false; }
+    if (x < 10) { return true; }
+
+    var temp = x;
+    var y = 0;
+    var digit = 0;
+    while (temp != 0)
     {
-        public  static bool IsPalindrome(int x)
-        {
-            if (x < 0) { return false; }
-            if (x < 10) { return true; }
-
-            var temp = x;
-            var y = 0;
-            var digit = 0;
-            while (temp != 0)
-            {
-                digit = temp % 10;
-                y = y * 10 + digit;
-                temp /= 10;
-            }
-
-            return x == y;
-        }
-
-        static void Main (string[] args)
-        {
-
-            var x = 121;
-            Console.WriteLine("Input: " + x);
-            var result = IsPalindrome(x);
-            Console.WriteLine("Output: " + result);
-        }
+        digit = temp % 10;
+        y = y * 10 + digit;
+        temp /= 10;
     }
+
+    return x == y;
 }
+
+var x = 121;
+Console.WriteLine("Input: " + x);
+var result = IsPalindrome(x);
+Console.WriteLine("Output: " + result);
