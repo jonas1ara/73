f8899ab50ce8ed22e07babd1fe135fe826734ca1
Jonas Lara <adrianlr2515@outlook.com>
2023-01-08 22:52:59 -0600

    38 - 39

--- 038.-CountAndSay/Program.cs
+++ 038.-CountAndSay/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string CountAndSay(int n)
+{
+    var result = "1";
+    char currentCh;
+    int i, j, startNum;
+    var builder = new StringBuilder();
+
+    for (i = 1; i < n; i++)
+    {
+        currentCh = result[0];
+        startNum = 0;
+        for (j = 1; j < result.Length; j++)
+        {
+            if (currentCh != result[j])
+            {
+                builder.Append(j - startNum);
+                builder.Append(currentCh);
+                currentCh = result[j];
+                startNum = j;
+            }
+        }
+        builder.Append(j - startNum);
+        builder.Append(currentCh);
+
+        result = builder.ToString();
+        builder.Clear();
+    }
+
+    return result;
+}
--- 039.-CombinationSum/Program.cs
+++ 039.-CombinationSum/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+
+string CountAndSay(int n)
+{
+    var result = "1";
+    char currentCh;
+    int i, j, startNum;
+    var builder = new StringBuilder();
+
+    for (i = 1; i < n; i++)
+    {
+        currentCh = result[0];
+        startNum = 0;
+        for (j = 1; j < result.Length; j++)
+        {
+            if (currentCh != result[j])
+            {
+                builder.Append(j - startNum);
+                builder.Append(currentCh);
+                currentCh = result[j];
+                startNum = j;
+            }
+        }
+        builder.Append(j - startNum);
+        builder.Append(currentCh);
+
+        result = builder.ToString();
+        builder.Clear();
+    }
+
+    return result;
+}
