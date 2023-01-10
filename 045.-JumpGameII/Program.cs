e03ec3e52c0e6d4f139d82debca446b39909c5a9
Jonas Lara <adrianlr2515@outlook.com>
2023-01-09 21:53:43 -0600

    UPDATE 45

--- 045.-JumpGameII/Program.cs
+++ 045.-JumpGameII/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+int Jump(int[] nums)
+{
+    int maxRight = 0, currentRight = 0, count = 0;
+
+    for (int i = 0; i < nums.Length; i++)
+    {
+        if (i > maxRight) { return 0; }
+        if (i > currentRight)
+        {
+            count++;
+            currentRight = maxRight;
+
+        }
+        if (i + nums[i] > maxRight) { maxRight = i + nums[i]; }
+    }
+
+    return count;
+}
