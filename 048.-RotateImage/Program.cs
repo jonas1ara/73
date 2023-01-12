9483753f35b7e14926b5a03a8ab0ce2e6f11b22a
Jonas Lara <adrianlr2515@ououtlook.com>
2023-01-11 22:56:13 -0600

    48 - 49 - 50

--- 048.-RotateImage/Program.cs
+++ 048.-RotateImage/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+void Rotate(int[,] matrix)
+{
+    int i, j, temp;
+    var lenght = matrix.GetLength(1);
+
+    for (i = 0; i < lenght / 2; i++)
+    {
+        for (j = i; j < lenght - i - 1; j++)
+        {
+            temp = matrix[i, j];
+            matrix[i, j] = matrix[lenght - j - 1, i];
+            matrix[lenght - j - 1, i] = matrix[lenght - i - 1, lenght - j - 1];
+            matrix[lenght - i - 1, lenght - j - 1] = matrix[j, lenght - i - 1];
+            matrix[j, lenght - i - 1] = temp;
+        }
+    }
