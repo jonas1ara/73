fc2cbcdf32d1ef09bc8dffdd529e9029f0c6c0b8
Jonas Lara <adrianlr2515@ououtlook.com>
2023-01-11 23:01:01 -0600

    N Queens I y N Queens II

--- 051.-N-Queens/Program.cs
+++ 051.-N-Queens/Program.cs
@@ -1,2 +1,51 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+string CONST_RESULT;
+
+IList<IList<string>> SolveNQueens(int n)
+{
+    CONST_RESULT = new string('.', n);
+
+    var results = new List<IList<string>>();
+    var queenColumns = new int[n];
+    RecursiveSolver(n, 0, queenColumns, results);
+
+    return results;
+}
+
+void RecursiveSolver(int n, int currentRow, int[] queenColumns, IList<IList<string>> results)
+{
+    if (currentRow == n)
+    {
+        var result = new List<string>();
+        StringBuilder rowString;
+        for (int i = 0; i < n; i++)
+        {
+            rowString = new StringBuilder(CONST_RESULT, n);
+            rowString[queenColumns[i]] = 'Q';
+            result.Add(rowString.ToString());
+        }
+        results.Add(result);
+        return;
+    }
+
+    bool isValid = true;
+    for (int col = 0; col < n; col++)
+    {
+        isValid = true;
+        for (int i = 0; i < currentRow; i++)
+        {
+            if (queenColumns[i] == col || Math.Abs(col - queenColumns[i]) == Math.Abs(currentRow - i))
+            {
+                isValid = false;
+                break;
+            }
+        }
+        if (!isValid) continue;
+
+        queenColumns[currentRow] = col;
+        RecursiveSolver(n, currentRow + 1, queenColumns, results);
+    }
+}
