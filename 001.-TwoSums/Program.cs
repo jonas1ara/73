16999adef6a0ddbaf91adcfe125d7ba33dbad921
Jonas Lara <adrianlr2515@outlook.com>
2023-01-14 22:42:36 -0600

    UPDATE Two Sms

--- 001.-TwoSums/Program.cs
+++ 001.-TwoSums/Program.cs
@@ -1,25 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 
-static int[] TwoSum(int[] nums, int target)
+namespace TwoSums
 {
-    var dic = new Dictionary<int, int>();
-
-    for (int i = 0; i < nums.Length; i++)
+    class Program
     {
-		int t = target - nums[i];
-        if (dic.ContainsKey(t)) return new int[] { dic[t], i };
-        dic[nums[i]] = i;
-    }
-    return new int[] { };
-}
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            var dic = new Dictionary<int, int>();
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                int t = target - nums[i];
+                if (dic.ContainsKey(t)) return new int[] { dic[t], i };
+                dic[nums[i]] = i;
+            }
+            return new int[] { };
+        }
     
-int[] nums = { 2, 1, 5, 3 };
-int target = 4;
-//Print the input
-Console.WriteLine("Input: nums = [" + String.Join(", ", nums) + "], target = " + target);
+        static void Main(string[] args)
+        {
+            int[] nums = { 2, 1, 5, 3 };
+            int target = 4;
+            
+            //Print the input
+            Console.WriteLine("Input: nums = [" + String.Join(", ", nums) + "], target = " + target);
 
-int[] result = TwoSum(nums, target);	
+            int[] result = TwoSum(nums, target);	
 
-//Print the output
-Console.WriteLine("Output: [" + String.Join(", ",result) + "]");
+            //Print the output
+            Console.WriteLine("Output: [" + String.Join(", ",result) + "]");
+        }
+    }
+}
