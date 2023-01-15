078c9c2b092bb53c3dea223cbf3f3a4bca945405
Jonas Lara <adrianlr2515@outlook.com>
2023-01-14 22:48:28 -0600

    namespace 003

--- 003.-LongestSubstringWithoutRepeatingCharacters/Program.cs
+++ 003.-LongestSubstringWithoutRepeatingCharacters/Program.cs
@@ -1,24 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 
-int LengthOfLongestSubstring(string s)
+namespace LengthOfLongesSubstring
 {
-        if (string.IsNullOrEmpty(s)) return 0;
-
-        var map = new Dictionary<int, int>();
-        var maxLen = 0;
-        var lastRepeatPos = -1;
-        for (int i = 0; i < s.Length; i++)
+    class Program
+    {
+        public static int LengthOfLongestSubstring(string s)
         {
-                if (map.ContainsKey(s[i]) && lastRepeatPos < map[s[i]])
-                    lastRepeatPos = map[s[i]];
-                if (maxLen < i - lastRepeatPos)
-                    maxLen = i - lastRepeatPos;
-                map[s[i]] = i;
-        }
-        return maxLen;
-}
+                if (string.IsNullOrEmpty(s)) return 0;
 
-string s = "abcabcbb";
-Console.WriteLine("Input: s = \"{0}\"", s);
-Console.WriteLine("Longest Substring Without Repeating Characters: " + LengthOfLongestSubstring(s));
+                var map = new Dictionary<int, int>();
+                var maxLen = 0;
+                var lastRepeatPos = -1;
+                for (int i = 0; i < s.Length; i++)
+                {
+                        if (map.ContainsKey(s[i]) && lastRepeatPos < map[s[i]])
+                            lastRepeatPos = map[s[i]];
+                        if (maxLen < i - lastRepeatPos)
+                            maxLen = i - lastRepeatPos;
+                        map[s[i]] = i;
+                }
+                return maxLen;
+        }
+
+        static void Main(string[] args)
+        {
+            string s = "abcabcbb";
+            Console.WriteLine("Input: s = \"{0}\"", s);
+            Console.WriteLine("Longest Substring Without Repeating Characters: " + LengthOfLongestSubstring(s));
+        }
+    }
+}
