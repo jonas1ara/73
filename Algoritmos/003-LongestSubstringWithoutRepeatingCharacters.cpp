b445d3e82b4250c0811ee84eb8ca685204ce2441
Jonas Lara <adrianlr2515@outlook.com>
2023-02-18 23:18:23 -0600

    Final 003

--- Algoritmos/003-LongestSubstringWithoutRepeatingCharacters.cpp
+++ Algoritmos/003-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -1,28 +1,28 @@
 #include <iostream>
-#include <string>
-#include <map>
+#include <vector>
 
 int lengthOfLongestSubstring(std::string s);
 
 int main()
 {
-    std::string s = "bbbb";
-    std::cout << "Input: '" << s << "'" << std::endl ;
+    std::string s = "bbbac";
+
+    std::cout << "Input: '" << s << "'" << std::endl;
 
     std::cout << "Output: " << lengthOfLongestSubstring(s) << std::endl;
 
     std::cin.get();
-} 
+}
 
 int lengthOfLongestSubstring(std::string s)
 {
     int ans = 0, start = -1;
-    std::map<int, int> map;
+    std::vector<int> m(128, -1);
     for (int i = 0; i < s.size(); ++i)
     {
-        start = std::max(start, map[s[i]]);
-        map[s[i]] = i;
-        ans = std::max(ans, i - start);
-    }
+        start = std::max(start, m[s[i]]); 
+        m[s[i]] = i;        
+        ans = std::max(ans, i - start); 
+    } 
     return ans;
 }
