38bd4aee7ee5d4c1c2efeaa79e8a08e791d96ca7
Jonas Lara <adrianlr2515@outlook.com>
2023-02-18 23:20:15 -0600

    Comment 003

--- Algoritmos/003-LongestSubstringWithoutRepeatingCharacters.cpp
+++ Algoritmos/003-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -16,12 +16,19 @@ int main()
 
 int lengthOfLongestSubstring(std::string s)
 {
-    int ans = 0, start = -1;
+    int ans = 0, start = -1; //start es el indice del primer caracter de la subcadena sin repetir
+    //Crear un vector de 128 posiciones con el valor -1 que servira para almacenar el indice de la ultima ocurrencia de cada caracter
     std::vector<int> m(128, -1);
+
     for (int i = 0; i < s.size(); ++i)
     {
+        //Si el caracter actual se ha repetido, actualizar el indice del primer caracter de la subcadena sin repetir
         start = std::max(start, m[s[i]]); 
+
+        //Actualizar el indice de la ultima ocurrencia del caracter actual
         m[s[i]] = i;        
+
+        //Actualizar el valor de la longitud de la subcadena sin repetir
         ans = std::max(ans, i - start); 
     } 
     return ans;
