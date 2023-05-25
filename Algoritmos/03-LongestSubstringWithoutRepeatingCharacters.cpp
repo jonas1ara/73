ee6a8f931985e98bb9ad603f65e28dde81744202
Jonas Lara <adrianlr2515@outlook.com>
2023-05-25 12:27:10 -0600

    Up

--- Algoritmos/03-LongestSubstringWithoutRepeatingCharacters.cpp
+++ Algoritmos/03-LongestSubstringWithoutRepeatingCharacters.cpp
@@ -5,22 +5,25 @@
 
 int lengthOfLongestSubstring(std::string s)
 {
-    int ans = 0, start = -1;
-    std::vector<int> m(128, -1);
-    for (int i = 0; i < s.size(); ++i)
+    int ans = 0;                 // Variable para almacenar la longitud de la subcadena más larga sin caracteres repetidos
+    int start = -1;              // Índice del inicio de la subcadena actual
+    std::vector<int> m(128, -1); // Vector para almacenar el índice más reciente de cada carácter ASCII en la cadena
+
+    for (int i = 0; i < s.size(); i++)
     {
-        start = std::max(start, m[s[i]]);
+        start = std::max(start, m[s[i]]); // Actualiza el inicio de la subcadena si se encuentra un carácter repetido
 
-        ans = std::max(ans, i - start);
+        ans = std::max(ans, i - start); // Calcula la longitud de la subcadena actual y la compara con la longitud máxima anterior
 
-        m[s[i]] = i;
+        m[s[i]] = i; // Actualiza el índice más reciente del carácter en el vector
     }
-    return ans;
+
+    return ans; // Devuelve la longitud de la subcadena más larga sin caracteres repetidos
 }
 
 int main()
 {
-    std::string s = " ";
+    std::string s = "abcabcbb";
     std::cout << "Input: '" << s << "'" << std::endl;
 
     std::cout << "Output: " << lengthOfLongestSubstring(s) << std::endl;
