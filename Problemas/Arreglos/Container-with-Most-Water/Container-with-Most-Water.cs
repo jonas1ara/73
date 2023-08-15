3f92d30006031dc3ea693af06bbb24fa8167e568
Jonas Lara <adrianlr2515@outlook.com>
2023-08-15 13:57:30 -0600

    UPDATE

---
+++ Problemas/Arreglos/Container-with-Most-Water/Container-with-Most-Water.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ContainerWithMostWater
+{
+    public class Solution 
+    {
+        public int MaxArea(int[] height)
+        {
+            int ans = 0, L = 0, R = height.Length - 1;
+            while (L < R)
+            {
+                ans = Math.Max(ans, (R - L) * Math.Min(height[L], height[R]));
+                if (height[L] < height[R]) L++; // Move the smaller edge
+                else R--;
+            }
+            return ans;
+        }
+        public static void Main(String[] args)
+        {
+            int[] height = new int[] {1,8,6,2,5,4,8,3,7};
+
+            Console.WriteLine("Input: height = [" + string.Join(" ", height) + "]"); 
+
+            int ans = new Solution().MaxArea(height);
+            Console.WriteLine("Output: " + ans);
+
+
+        }
+    }   
+}
--- Problemas/Arreglos/Two-Sum/Two-Sum.cs
+++ Problemas/Arreglos/Two-Sum/Two-Sum.cs
@@ -5,18 +5,6 @@ namespace TwoSum
 {
 	class Program
 	{
-		public static void Main(String[] args)
-		{
-			int[] nums = { 2, 7, 11, 15 };
-			int target = 9;
-			int[] result = TwoSum(nums, target);
-
-			foreach (var item in result)
-			{
-				Console.Write(item + " ");
-			}
-			Console.WriteLine();
-		}
 		public static int[] TwoSum(int[] nums, int target)
 		{
 			var dic = new Dictionary<int, int>();
@@ -31,6 +19,18 @@ namespace TwoSum
 				dic[nums[i]] = i; 
 			}
 			return new int[] { }; //If the sum has no solution, return the empty array
+		}
+		public static void Main(String[] args)
+		{
+			int[] nums = { 2, 7, 11, 15 };
+			int target = 9;
+			int[] result = TwoSum(nums, target);
+
+			foreach (var item in result)
+			{
+				Console.Write(item + " ");
+			}
+			Console.WriteLine();
 		}
 	}
 }
