382f2ce955e3f73d945ff15e1e80c043ab97dee2
Jonas Lara <adrianlr2515@outlook.com>
2023-08-14 22:33:52 -0600

    UPDATE

---
+++ Problemas/Arreglos/Two-Sum/Two-Sum.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TwoSum
+{
+	class Program
+	{
+		public static void Main(String[] args)
+		{
+			int[] nums = { 2, 7, 11, 15 };
+			int target = 9;
+			int[] result = TwoSum(nums, target);
+
+			foreach (var item in result)
+			{
+				Console.Write(item + " ");
+			}
+			Console.WriteLine();
+		}
+		public static int[] TwoSum(int[] nums, int target)
+		{
+			var dic = new Dictionary<int, int>();
+
+			for (int i = 0; i < nums.Length; i++)
+			{
+				int t = target - nums[i]; 
+				
+				//If the dictionary contains the key t, return the index of t and the current index
+				if (dic.ContainsKey(t)) return new int[] { dic[t], i }; 
+				
+				dic[nums[i]] = i; 
+			}
+			return new int[] { }; //If the sum has no solution, return the empty array
+		}
+	}
+}
+
+
+
+
+
