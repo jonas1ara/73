d6e88baa58cd1ac99be0c4b2a705e4320ba750fe
Jonas Lara <adrianlr2515@outlook.com>
2023-10-13 21:57:32 -0600

    UPDATE

--- Problems/Strings/Minimum-Window-Substring/Minimum-Window-Substring.cpp
+++ Problems/Strings/Minimum-Window-Substring/Minimum-Window-Substring.cpp
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#include <iostream>
+#include <string>
+#include <climits>
+
+class Solution {
+public:
+    std::string minWindow(std::string s, std::string t) {
+        int cnt[128] = {};
+        for (char c : t)
+            cnt[c]++;
+        int N = s.size(), i = 0, j = 0, start = -1, minLen = INT_MAX, matched = 0;
+        while (j < N) {
+            matched += --cnt[s[j++]] >= 0;
+            while (matched == t.size()) {
+                if (j - i < minLen)
+                    minLen = j - i, start = i;
+                matched -= ++cnt[s[i++]] > 0;
+            }
+        }
+        return start == -1 ? "" : s.substr(start, minLen);
+    }
+};
+
+int main() {
+    Solution solution;
+    std::string s = "ADOBECODEBANC";
+    std::string t = "ABC";
+
+    std::string result = solution.minWindow(s, t);
+
+    std::cout << "Ventana mínima: " << result << std::endl;
+
+    return 0;
+}
---
+++ Problems/Strings/Minimum-Window-Substring/Minimum-Window-Substring.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+public class Solution {
+    public string MinWindow(string s, string t) {
+        int[] cnt = new int[128];
+        foreach (char c in t) {
+            cnt[c]++;
+        }
+
+        int N = s.Length;
+        int i = 0, j = 0, start = -1, minLen = int.MaxValue, matched = 0;
+
+        while (j < N) {
+            if (cnt[s[j]] > 0) {
+                matched++;
+            }
+            cnt[s[j]]--;
+            j++;
+
+            while (matched == t.Length) {
+                if (j - i < minLen) {
+                    minLen = j - i;
+                    start = i;
+                }
+
+                if (cnt[s[i]] == 0) {
+                    matched--;
+                }
+                cnt[s[i]]++;
+                i++;
+            }
+        }
+
+        return start == -1 ? "" : s.Substring(start, minLen);
+    }
+    public static void Main(string[] args) {
+        Solution solution = new Solution();
+
+        string s = "ADOBECODEBANC";
+        string t = "ABC";
+
+        string result = solution.MinWindow(s, t);
+
+        Console.WriteLine("Ventana mínima: " + result);
+    }
+}
