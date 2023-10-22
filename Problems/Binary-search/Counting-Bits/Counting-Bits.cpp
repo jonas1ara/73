a22aa79b00aa6419580993a7988de57a6d331d25
Jonas Lara <adrianlr2515@outlook.com>
2023-10-22 02:43:51 -0600

    UP

--- Problems/Binary-search/Counting-Bits/Counting-Bits.cpp
+++ Problems/Binary-search/Counting-Bits/Counting-Bits.cpp
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// int arr[] = {2, 5, 7, 10, 14, 18, 20};
+// int n = sizeof(arr) / sizeof(arr[0]);
+// int target = 10;
+
+int binarySearch(int arr[], int left, int right, int target)
+{
+    if (right >= left)
+    {
+        int half = left + (right - left) / 2;
+
+        if (arr[half] == target)
+        {
+            return half;
+        }
+
+        if (arr[half] > target)
+        {
+            return binarySearch(arr, left, half - 1, target);
+        }
+
+        return binarySearch(arr, half + 1, right, target);
+    }
+
+    return -1; 
+}
