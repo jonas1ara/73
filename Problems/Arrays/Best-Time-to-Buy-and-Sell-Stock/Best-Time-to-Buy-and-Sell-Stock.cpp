49998376e3671c55e9702f7e69df464f923a6272
Jonas Lara <adrianlr2515@outlook.com>
2023-11-29 13:55:14 -0600

    Best Time to Buy and Sell Stock code completed

--- Problems/Arrays/Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
+++ Problems/Arrays/Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cpp
@@ -1,25 +1,46 @@
-﻿#include "../../../Libraries/ArrayPrinter.h"
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 #include <climits>
 
+// Using dynamic programming technique - Time: O(n)
+
 class Solution {
 public:
-    int maxProfit(std::vector<int> &A)
+    int maxProfit(std::vector<int> &prices)
     {
         int buy = INT_MIN, sell = 0;
-        for (int n : A)
+        
+        for (int price : prices)
         {
-            buy = std::max(buy, -n);
-            sell = std::max(sell, buy + n);
+            buy = std::max(buy, -price);
+            sell = std::max(sell, buy + price);
         }
+
         return sell;
     }
 };
 
 int main()
 {
-    std::vector<int> array = {7, 1, 5, 3, 6, 4};
+    std::vector<int> prices = {7, 1, 5, 3, 6, 4};
+
+    // Print input
+    std::cout << "Input: prices = [";
+    for (const auto &price : prices)
+    {
+        std::cout << price << "";
+        if (&price != &prices.back())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    Solution sol;
+    int maxProfit = sol.maxProfit(prices);
+
+    // Print output
+    std::cout << "Output: " << maxProfit << std::endl;
 
     return 0;
 }
--- Problems/Arrays/Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cs
+++ Problems/Arrays/Best-Time-to-Buy-and-Sell-Stock/Best-Time-to-Buy-and-Sell-Stock.cs
@@ -1,23 +1,45 @@
 ﻿using System;
+
+// Using dynamic programming technique - Time: O(n)
+
 public class Solution
 {
-    public int MaxProfit(int[] A)
+    public int MaxProfit(int[] prices)
     {
-        int buy = int.MinValue;
-        int sell = 0;
+        int buy = int.MinValue, sell = 0;
 
-        foreach (int n in A)
+        foreach (int price in prices)
         {
-            buy = Math.Max(buy, -n);
-            sell = Math.Max(sell, buy + n);
+            buy = Math.Max(buy, -price);
+            sell = Math.Max(sell, buy + price);
         }
 
         return sell;
     }
+}
 
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
-        int[] nums = new int[] { 7, 1, 5, 3, 6, 4 };
-        Console.WriteLine(new Solution().MaxProfit(nums));
+        int[] prices = { 7, 1, 5, 3, 6, 4 };
+
+        // Print input
+        Console.Write("Input: prices = [");
+        foreach (int price in prices)
+        {
+            Console.Write(price + "");
+            if (price != prices[prices.Length - 1])
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        Solution solution = new Solution();
+        int maxProfit = solution.MaxProfit(prices);
+
+        // Print output
+        Console.WriteLine("Output: " + maxProfit);
     }
 }
