23f6a1349ad37079f546e80df7f1523e45033a42
Jonas Lara <adrianlr2515@outlook.com>
2023-11-30 10:51:00 -0600

    Find Minimum in Rotated Sorted Array code completed

--- Problems/Arrays/Find-Minimum-in-Rotated-Sorted-Array/Find-Minimum-in-Rotated-Sorted-Array.cpp
+++ Problems/Arrays/Find-Minimum-in-Rotated-Sorted-Array/Find-Minimum-in-Rotated-Sorted-Array.cpp
@@ -1,27 +1,47 @@
-﻿#include "../../../Libraries/ArrayPrinter.h"
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+
+// Using binary search technique - Time O(log n)
 
 class Solution {
 public:
     int findMin(std::vector<int> &nums)
     {
-        int L = 0, R = nums.size() - 1;
-        while (L < R)
+        int left = 0, right = nums.size() - 1;
+
+        while (left < right)
         {
-            int M = (L + R) / 2;
-            if (nums[M] > nums[R])
-                L = M + 1;
+            int m = (left + right) / 2; // middle index
+
+            if (nums[m] > nums[right])
+                left = m + 1;
             else
-                R = M;
+                right = m;
         }
-        return nums[L];
+
+        return nums[left];
     }
 };
 
 int main()
 {
     std::vector<int> nums = {3, 4, 5, 1, 2};
+
+    // Print input
+    std::cout << "Input: nums = [";
+    for (int i = 0; i < nums.size(); i++)
+    {
+        std::cout << nums[i] << "";
+        if (i < nums.size() - 1)
+            std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
+
+    Solution sol;
+    int result = sol.findMin(nums);
+
+    // Print output
+    std::cout << "Output: " << result << std::endl;
 
     return 0;
 }
--- Problems/Arrays/Find-Minimum-in-Rotated-Sorted-Array/Find-Minimum-in-Rotated-Sorted-Array.cs
+++ Problems/Arrays/Find-Minimum-in-Rotated-Sorted-Array/Find-Minimum-in-Rotated-Sorted-Array.cs
@@ -1,32 +1,53 @@
 ﻿using System;
 
+// Using binary search technique - Time O(log n)
+
 public class Solution
 {
     public int FindMin(int[] nums)
     {
-        int L = 0;
-        int R = nums.Length - 1;
+        int left = 0, right = nums.Length - 1;
 
-        while (L < R)
+        while (left < right)
         {
-            int M = (L + R) / 2;
+            int m = (left + right) / 2; // middle index
 
-            if (nums[M] > nums[R])
+            if (nums[m] > nums[right])
             {
-                L = M + 1;
+                left = m + 1;
             }
             else
             {
-                R = M;
+                right = m;
             }
         }
 
-        return nums[L];
+        return nums[left];
     }
+}
 
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
         int[] nums = new int[] { 3, 4, 5, 1, 2 };
-        Console.WriteLine(new Solution().FindMin(nums));
+
+        // Print input
+        Console.Write("Input: nums = [");
+        for (int i = 0; i < nums.Length; i++)
+        {
+            Console.Write(nums[i] + "");
+            if (i < nums.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        Solution sol = new Solution();
+        int result = sol.FindMin(nums);
+
+        // Print output
+        Console.WriteLine("Output " + result);
     }
 }
