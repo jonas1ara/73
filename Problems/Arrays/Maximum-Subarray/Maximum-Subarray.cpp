ef5b34829644a0f560748ad3c6f4abc6b65c942f
Jonas Lara <adrianlr2515@outlook.com>
2023-11-29 18:36:41 -0600

    Maximum Subarray code completed

--- Problems/Arrays/Maximum-Subarray/Maximum-Subarray.cpp
+++ Problems/Arrays/Maximum-Subarray/Maximum-Subarray.cpp
@@ -1,24 +1,45 @@
-﻿#include "../../../Libraries/ArrayPrinter.h"
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
+
+// Using Kadan's algorithm - Time: O(n)
 
 class Solution {
 public:
     int maxSubArray(std::vector<int> &nums)
     {
         int ans = nums[0];
-        for (int i = 1; i < nums.size(); ++i)
+
+        for (int i = 1; i < nums.size(); i++)
         {
             nums[i] += std::max(nums[i - 1], 0);
             ans = std::max(ans, nums[i]);
         }
+
         return ans;
     }
 };
 
 int main()
 {
-    std::vector<int> array = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    std::vector<int> nums = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+
+    // Print input
+    std::cout << "Input: nums = [";
+    for (const auto &num : nums)
+    {
+        std::cout << num << "";
+        if (&num != &nums.back())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    Solution sol;
+    int result = sol.maxSubArray(nums);
+
+    // Print output
+    std::cout << "Output: " << result << std::endl;
 
     return 0;
 }
--- Problems/Arrays/Maximum-Subarray/Maximum-Subarray.cs
+++ Problems/Arrays/Maximum-Subarray/Maximum-Subarray.cs
@@ -1,20 +1,44 @@
 ﻿using System;
+
+// Using Kadan's algorithm - Time: O(n)
 public class Solution
 {
     public int MaxSubArray(int[] nums)
     {
         int ans = nums[0];
-        for (int i = 1; i < nums.Length; ++i)
+
+        for (int i = 1; i < nums.Length; i++)
         {
             nums[i] += Math.Max(nums[i - 1], 0);
             ans = Math.Max(ans, nums[i]);
         }
+
         return ans;
     }
+}
 
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
-        int[] nums = new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
-        Console.WriteLine(new Solution().MaxSubArray(nums));
+        int[] nums = { -2, 1, -3, 4, -1, 2, 1, -5, 4 };
+
+        // Print input
+        Console.Write("Input: nums = [");
+        foreach (int num in nums)
+        {
+            Console.Write(num + "");
+            if (num != nums[nums.Length - 1])
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        Solution sol = new Solution();
+        int result = sol.MaxSubArray(nums);
+
+        // Print output
+        Console.WriteLine("Output: " + result);
     }
 }
