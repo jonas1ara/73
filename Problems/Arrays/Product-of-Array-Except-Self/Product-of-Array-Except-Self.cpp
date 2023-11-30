0dd060c99c2698efdd4ea1e7cd6a4ac125145638
Jonas Lara <adrianlr2515@outlook.com>
2023-11-30 12:08:25 -0600

    Product of Array Except Self code completed

--- Problems/Arrays/Product-of-Array-Except-Self/Product-of-Array-Except-Self.cpp
+++ Problems/Arrays/Product-of-Array-Except-Self/Product-of-Array-Except-Self.cpp
@@ -1,26 +1,26 @@
-﻿#include "../../../Libraries/ArrayPrinter.h"
-#include <iostream>
+﻿#include <iostream>
 #include <vector>
 
-class Solution
-{
+// Using prefix and postfix - Time O(n)
+
+class Solution {
 public:
     std::vector<int> productExceptSelf(std::vector<int> &nums)
     {
-        int N = nums.size();
-        std::vector<int> result(N, 1);
+        int n = nums.size();
+        std::vector<int> result(n, 1);
         result[0] = 1;
 
-        for (int i = 1; i < N; i++)
+        for (int i = 1; i < n; i++)
         {
             result[i] = result[i - 1] * nums[i - 1];
         }
 
         int rightSide = 1;
-        for (int i = N - 1; i >= 0; i--)
+        for (int i = n - 1; i >= 0; i--)
         {
             result[i] = result[i] * rightSide;
-            rightSide *= nums[i];
+            rightSide *= nums[i]; // rightSide = rightSide * nums[i];
         }
 
         return result;
@@ -29,7 +29,34 @@ public:
 
 int main()
 {
-    std::vector<int> array = {1, 2, 3, 4};
+    std::vector<int> nums = {1, 2, 3, 4};
+
+    // Print input
+    std::cout << "Input: nums = [";
+    for (const auto &num : nums)
+    {
+        std::cout << num << "";
+        if (&num != &nums.back())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
+
+    Solution sol;
+    std::vector<int> result = sol.productExceptSelf(nums);
+
+    // Print input
+    std::cout << "Output: [";
+    for (const auto &r : result)
+    {
+        std::cout << r << "";
+        if (&r != &result.back())
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]" << std::endl;
 
     return 0;
 }
--- Problems/Arrays/Product-of-Array-Except-Self/Product-of-Array-Except-Self.cs
+++ Problems/Arrays/Product-of-Array-Except-Self/Product-of-Array-Except-Self.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 
+// Using prefix and postfix - Time O(n)
 public class Solution
 {
     public int[] ProductExceptSelf(int[] nums)
@@ -8,23 +9,52 @@ public class Solution
         result[0] = 1;
 
         for (int i = 1; i < nums.Length; i++)
+        {
             result[i] = result[i - 1] * nums[i - 1];
+        }
 
         int rightSide = 1;
         for (int i = nums.Length - 1; i >= 0; i--)
         {
             result[i] = result[i] * rightSide;
-            rightSide *= nums[i];
+            rightSide *= nums[i]; // rightSide = rightSide * nums[i];
         }
 
         return result;
     }
-    public static void Main(string[] args)
-    {
-        var solution = new Solution();
-        var result = solution.ProductExceptSelf(new int[] { 1, 2, 3, 4 });
-        foreach (var item in result)
-            Console.WriteLine(item);
-    }
+}
 
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 1, 2, 3, 4 };
+
+        // Print intput
+        Console.Write("Input: nums = [");
+        foreach (int n in nums)
+        {
+            Console.Write(n + "");
+            if (n != nums[nums.Length - 1])
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        Solution sol = new Solution();
+        int[] result = sol.ProductExceptSelf(nums);
+
+        // Print intput
+        Console.Write("Output: [");
+        foreach (int r in result)
+        {
+            Console.Write(r + "");
+            if (r != result[result.Length - 1])
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+    }
 }
