05394bd321ec382a060ec077326eb34b4e543161
Jonas Lara <adrianlr2515@outlook.com>
2023-12-07 15:28:59 -0600

    Rotate-Image code completed

--- Problems/Matrix/Rotate-Image/Rotate-Image.cs
+++ Problems/Matrix/Rotate-Image/Rotate-Image.cs
@@ -3,21 +3,25 @@ public class Solution
 {
     public void Rotate(int[][] matrix)
     {
-        int N = matrix.Length;
-        for (int i = 0; i < N / 2; ++i)
+        int n = matrix.Length;
+
+        for (int i = 0; i < n / 2; i++)
         {
-            for (int j = i; j < N - i - 1; ++j)
+            for (int j = i; j < n - i - 1; j++)
             {
                 int tmp = matrix[i][j];
-                matrix[i][j] = matrix[N - j - 1][i];
-                matrix[N - j - 1][i] = matrix[N - i - 1][N - j - 1];
-                matrix[N - i - 1][N - j - 1] = matrix[j][N - i - 1];
-                matrix[j][N - i - 1] = tmp;
+                matrix[i][j] = matrix[n - j - 1][i];
+                matrix[n - j - 1][i] = matrix[n - i - 1][n - j - 1];
+                matrix[n - i - 1][n - j - 1] = matrix[j][n - i - 1];
+                matrix[j][n - i - 1] = tmp;
             }
         }
     }
+}
 
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
         // Jagged array
         int[][] matrix = {
@@ -38,7 +42,7 @@ public class Solution
         }
 
         Console.WriteLine("After rotate:");
-        
+
         new Solution().Rotate(matrix);
         foreach (int[] row in matrix)
         {
