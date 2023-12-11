41f39fb587888f1596c96a3696356eb8f8898c7d
Jonas Lara <adrianlr2515@outlook.com>
2023-12-11 12:59:58 -0600

    Valid-Anagram code completed

--- Problems/Strings/Valid-Anagram/Valid-Anagram.cpp
+++ Problems/Strings/Valid-Anagram/Valid-Anagram.cpp
@@ -1,9 +1,38 @@
 ﻿#include <iostream>
-using namespace std;
+#include <string>
 
-int main()
+class Solution {
+public:
+    bool isAnagram(std::string s, std::string t)
+    {
+        int cnt[26] = {};
+
+        for (char c : s)
+            cnt[c - 'a']++;
+
+        for (char c : t)
+            cnt[c - 'a']--;
+
+        for (int n : cnt)
+        {
+            if (n)
+                return false;
+        }
+
+        return true;
+    }
+};
+
+int main() 
 {
-    cout <<  "¯  _(ツ)_/¯ " <<endl; 
-    
+    std::string s = "anagram";
+    std::string t = "nagaram";
+    std::cout << "Input: s = " << s << ", t = " << t << std::endl;
+
+    Solution sol;
+    bool result = sol.isAnagram(s, t);
+
+    std::cout << "Output = " << std::boolalpha << result << std::endl;
+
     return 0;
 }
--- Problems/Strings/Valid-Anagram/Valid-Anagram.cs
+++ Problems/Strings/Valid-Anagram/Valid-Anagram.cs
@@ -1,17 +1,16 @@
-﻿public class Solution
+﻿using System;
+
+public class Solution
 {
     public bool IsAnagram(string s, string t)
     {
         int[] cnt = new int[26];
+
         foreach (char c in s)
-        {
             cnt[c - 'a']++;
-        }
 
         foreach (char c in t)
-        {
             cnt[c - 'a']--;
-        }
 
         foreach (int n in cnt)
         {
@@ -22,5 +21,20 @@
         }
 
         return true;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "anagram";
+        string t = "nagaram";
+        Console.WriteLine("Input: s = " + s + ", t = " + t);
+
+        Solution sol = new Solution();
+        bool result = sol.IsAnagram(s, t);
+
+        Console.WriteLine("Output: " + result);
     }
 }
