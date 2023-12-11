f95850deaa8a8681b8f00db74779d526783b9850
Jonas Lara <adrianlr2515@outlook.com>
2023-12-11 10:05:01 -0600

    Valid-Palindrome code completed

--- Problems/Strings/Valid-Palindrome/Valid-Palindrome.cpp
+++ Problems/Strings/Valid-Palindrome/Valid-Palindrome.cpp
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿#include <iostream>
+
+// Using two pointers technique - Time: O(n)
+
+class Solution {
+public:
+    bool isPalindrome(std::string s)
+    {
+        int i = 0, j = s.length() - 1;
+        
+        while (i < j)
+        {
+            while (i < j && !isalnum(s[i]))
+                i++;
+            while (i < j && !isalnum(s[j]))
+                j--;
+            if (i < j && tolower(s[i]) != tolower(s[j]))
+                return false;
+            
+            i++;
+            j--;
+        }
+
+        return true;
+    }
+};
+
+int main()
+{
+    std::string s = "A man, a plan, a canal, Panama!";
+    std::cout << "Input: s = " << s << std::endl;
+
+    Solution sol;
+    bool result = sol.isPalindrome(s);
+
+    std::cout <<  "Output: "  << std::boolalpha << result << std::endl;
+
+    return 0;
+}
--- Problems/Strings/Valid-Palindrome/Valid-Palindrome.cs
+++ Problems/Strings/Valid-Palindrome/Valid-Palindrome.cs
@@ -1,16 +1,39 @@
-﻿public class Solution
+﻿using System;
+
+// Using two pointers technique - Time: O(n)
+public class Solution
 {
     public bool IsPalindrome(string s)
     {
         int i = 0, j = s.Length - 1;
+        
         while (i < j)
         {
-            while (i < j && !char.IsLetterOrDigit(s[i])) ++i;
-            while (i < j && !char.IsLetterOrDigit(s[j])) --j;
-            if (i < j && char.ToLower(s[i]) != char.ToLower(s[j])) return false;
+            while (i < j && !char.IsLetterOrDigit(s[i]))
+                i++;
+            while (i < j && !char.IsLetterOrDigit(s[j]))
+                j--;
+            if (i < j && char.ToLower(s[i]) != char.ToLower(s[j]))
+                return false;
+
             i++;
             j--;
         }
+
         return true;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string s = "A man, a plan, a canal, Panama!";
+        Console.WriteLine("Input: s = " + s);
+
+        Solution sol = new Solution();
+        bool result = sol.IsPalindrome(s);
+        
+        Console.WriteLine("Output: " + result);
     }
 }
