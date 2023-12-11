df1b95099002077afe95b68c7dd028f73f6b6623
Jonas Lara <adrianlr2515@outlook.com>
2023-12-11 16:19:57 -0600

    Counting-Bits code completed

--- Problems/Binary/Counting-Bits/Counting-Bits.cpp
+++ Problems/Binary/Counting-Bits/Counting-Bits.cpp
@@ -1,18 +1,41 @@
 ﻿#include <iostream>
 #include <vector>
 
-class Solution
-{
+//  Using dynamic programming - Time: O(n)
+
+class Solution {
 public:
     std::vector<int> countBits(int n)
     {
         std::vector<int> ans(n + 1);
+
         for (int i = 1; i <= n; i *= 2)
         {
             ans[i] = 1;
-            for (int j = 1; j < i && i + j <= n; ++j)
+
+            for (int j = 1; j < i && i + j <= n; j++)
                 ans[i + j] = ans[i] + ans[j];
         }
+
         return ans;
     }
 };
+
+int main()
+{
+    int n = 5; 
+    std::cout << "Input: n = " << n << std::endl;
+
+    Solution sol;
+    std::vector<int> result = sol.countBits(n);
+
+    std::cout << "Output: [";
+    for (int i = 0; i <= n; i++)
+    {
+        std::cout << result[i];
+        if (i < n) std::cout << ", ";
+    }
+    std::cout << "]\n";
+
+    return 0;
+}
--- Problems/Binary/Counting-Bits/Counting-Bits.cs
+++ Problems/Binary/Counting-Bits/Counting-Bits.cs
@@ -1,24 +1,34 @@
 ﻿using System;
 
+//  Using dynamic programming - Time: O(n)
 public class Solution
 {
     public int[] CountBits(int n)
     {
         int[] ans = new int[n + 1];
+
         for (int i = 1; i <= n; i *= 2)
         {
             ans[i] = 1;
-            for (int j = 1; j < i && i + j <= n; ++j)
-            {
+            
+            for (int j = 1; j < i && i + j <= n; j++)
                 ans[i + j] = ans[i] + ans[j];
-            }
         }
+
         return ans;
     }
+}
 
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
         int n = 5;
-        Console.WriteLine(string.Join(", ", new Solution().CountBits(n)));
+        Console.WriteLine("Input: n = " + n);
+
+        Solution sol = new Solution();
+        int[] ans = sol.CountBits(n);
+
+        Console.WriteLine("Output: [" + string.Join(", ", ans) + "]");
     }
 }
