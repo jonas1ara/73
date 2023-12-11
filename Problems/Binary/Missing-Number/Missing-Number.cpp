3b3a12245745a87279e73818f79fb568197a23d5
Jonas Lara <adrianlr2515@outlook.com>
2023-12-11 16:07:47 -0600

    Missing-Number code completed

--- Problems/Binary/Missing-Number/Missing-Number.cpp
+++ Problems/Binary/Missing-Number/Missing-Number.cpp
@@ -1,14 +1,39 @@
 ﻿#include <iostream>
 #include <vector>
 
-class Solution
-{
+// Using XOR operation - Time: O(n)
+
+class Solution {
 public:
     int missingNumber(std::vector<int> &nums)
     {
-        int n = nums.size(), xorVal = 0;
+        int n = nums.size(); 
+        int xorVal = 0;
+        
         for (int i = 0; i < n; ++i)
             xorVal ^= nums[i] ^ (i + 1);
+
         return xorVal;
     }
 };
+
+int main()
+{
+    std::vector<int> nums = {3, 0, 1}; 
+    
+    Solution sol;
+    int ans = sol.missingNumber(nums);
+
+    std::cout << "Input: nums = [";
+    for (int num : nums) 
+    {
+        std::cout << num << "";
+        if (num != nums.back())
+            std::cout << ", ";
+    }
+    std::cout << "]\n";
+
+    std::cout << "Output: " << ans << std::endl;
+
+    return 0;
+}
--- Problems/Binary/Missing-Number/Missing-Number.cs
+++ Problems/Binary/Missing-Number/Missing-Number.cs
@@ -1,17 +1,29 @@
 ﻿using System;
 
-public class Solution {
-    public int MissingNumber(int[] nums) {
+// Using XOR operation - Time: O(n)
+public class Solution
+{
+    public int MissingNumber(int[] nums)
+    {
         int n = nums.Length;
         int xorVal = 0;
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < n; i++)
             xorVal ^= nums[i] ^ (i + 1);
-        }
+
         return xorVal;
     }
+}
 
-    public static void Main(string[] args) {
-        int[] nums = new int[] { 3, 0, 1 };
-        Console.WriteLine(new Solution().MissingNumber(nums));
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] nums = { 3, 0, 1 };
+        Console.WriteLine("Input: nums = " + string.Join(", ", nums));
+
+        Solution sol = new Solution();  
+        int ans = sol.MissingNumber(nums);
+
+        Console.WriteLine("Output: " + ans);
     }
 }
