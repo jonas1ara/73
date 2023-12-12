20cbf0e0fa0c66a2d131e534fedb385ccdb5adcc
Jonas Lara <adrianlr2515@outlook.com>
2023-12-12 14:48:56 -0600

    Non-overlapping-Intervals code completed

--- Problems/Intervals/Non-overlapping-Intervals/Non-overlapping-Intervals.cpp
+++ Problems/Intervals/Non-overlapping-Intervals/Non-overlapping-Intervals.cpp
@@ -3,31 +3,46 @@
 #include <algorithm>
 #include <climits>
 
-class Solution
-{
+// Using a greedy algorithm - Time: O(nlogn)
+
+class Solution {
 public:
-    int eraseOverlapIntervals(std::vector<std::vector<int>> &A)
+    int eraseOverlapIntervals(std::vector<std::vector<int>> &intervals)
     {
-        sort(begin(A), end(A), [](auto &a, auto &b)
-             { return a[1] < b[1]; });
-        int ans = 0, e = INT_MIN;
-        for (auto &v : A)
+        sort(begin(intervals), end(intervals), [](auto &a, auto &b) { return a[1] < b[1]; });
+
+        int ans = 0;
+        int end = INT_MIN;
+
+        for (auto &i : intervals)
         {
-            if (v[0] >= e)
-                e = v[1];
+            if (i[0] >= end)
+                end = i[1];
             else
-                ++ans;
+                ans++;
         }
+
         return ans;
     }
 };
 
-int main() {
-    Solution solution;
-    std::vector<std::vector<int>> intervals = {{1, 3}, {2, 4}, {3, 5}, {4, 6}};
-    int result = solution.eraseOverlapIntervals(intervals);
+int main() 
+{    
+    std::vector<std::vector<int>> intervals = {{1, 2}, {2, 3}, {3, 4}, {1, 3}};
+
+    std::cout << "Input: intervals = [";
+    for (const auto &interval : intervals)
+    {
+        std::cout << "[" << interval[0] << "," << interval[1] << "]";
+        if (&interval != &intervals.back())
+            std::cout << ",";
+    }
+    std::cout << "]" << std::endl;
     
-    std::cout << "Minimum number of intervals to remove: " << result << std::endl;
+    Solution sol;
+    int ans = sol.eraseOverlapIntervals(intervals);
+    
+    std::cout << "Output: " << ans << std::endl;
     
     return 0;
 }
--- Problems/Intervals/Non-overlapping-Intervals/Non-overlapping-Intervals.cs
+++ Problems/Intervals/Non-overlapping-Intervals/Non-overlapping-Intervals.cs
@@ -1,39 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
+
+// Using a greedy algorithm - Time: O(nlogn)
 
 public class Solution
 {
-    public int EraseOverlapIntervals(int[][] A)
+    public int EraseOverlapIntervals(int[][] intervals)
     {
-        Array.Sort(A, (a, b) => a[1].CompareTo(b[1]));
-        int ans = 0;
-        int e = int.MinValue;
+        Array.Sort(intervals, (a, b) => a[1].CompareTo(b[1]));
 
-        foreach (var v in A)
+        int ans = 0;
+        int end = int.MinValue; // end 
+
+        foreach (var i in intervals)
         {
-            if (v[0] >= e)
-                e = v[1];
+            if (i[0] >= end)
+                end = i[1];
             else
                 ans++;
         }
 
         return ans;
     }
+}
 
-    public static void Main(string[] args)
+class Program
+{
+    static void Main(string[] args)
     {
-        Solution solution = new Solution();
         int[][] intervals = new int[][] {
-            new int[] {1, 3},
-            new int[] {2, 4},
-            new int[] {3, 5},
-            new int[] {4, 6}
+            new int[] {1, 2},
+            new int[] {2, 3},
+            new int[] {3, 4},
+            new int[] {1, 3}
         };
+        
+        Console.Write("Input: intervals = [");
+        foreach (var i in intervals)
+        {
+            Console.Write("[" + i[0] + "," + i[1] + "]");
+            if(i != intervals[intervals.Length - 1])
+                Console.Write(",");
+        }
+        Console.WriteLine("]");
 
-        int result = solution.EraseOverlapIntervals(intervals);
+        Solution sol = new Solution();
+        int ans = sol.EraseOverlapIntervals(intervals);
 
-        Console.WriteLine("Minimum number of intervals to remove: " + result);
+        Console.WriteLine("Output: " + ans);
     }
 }
 
