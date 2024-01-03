42cf83a06a29afa2eb43323a7f25193f2d45ec36
Jonas Lara <adrianlr2515@outlook.com>
2024-01-03 13:59:38 -0600

    Merge-k-Sorted-Lists code completed

--- Problems/Linked-lists/Merge-k-Sorted-Lists/Merge-k-Sorted-Lists.cs
+++ Problems/Linked-lists/Merge-k-Sorted-Lists/Merge-k-Sorted-Lists.cs
@@ -1,19 +1,21 @@
-﻿/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
+﻿using System;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution
 {
     private ListNode MergeTwoLists(ListNode a, ListNode b)
     {
-        ListNode head = new ListNode(0); 
+        ListNode head = new ListNode(0);
         ListNode tail = head;
         while (a != null && b != null)
         {
@@ -48,5 +50,51 @@ public class Solution
             }
         }
         return lists[0];
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        ListNode list1 = new ListNode(1, new ListNode(4, new ListNode(5)));
+        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(6)));
+        ListNode list3 = new ListNode(2, new ListNode(7, new ListNode(8)));
+
+        ListNode[] lists = { list1, list2, list3 };
+
+        Console.Write("Input: [");
+        for (int i = 0; i < lists.Length; i++)
+        {
+            PrintList(lists[i]);
+            if (i < lists.Length - 1)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.WriteLine("]");
+
+        Solution sol = new Solution();
+        ListNode mergedList = sol.MergeKLists(lists);
+
+        Console.Write("Output: ");
+        PrintList(mergedList);
+        Console.WriteLine();
+    }
+
+    static void PrintList(ListNode head)
+    {
+        Console.Write("[");
+        while (head != null)
+        {
+            Console.Write(head.val);
+            head = head.next;       
+            if (head != null)
+            {
+                Console.Write(", ");
+            }     
+        }
+        Console.Write("]");
     }
 }
