37241abf436c54d279757e896a71f4c5fba8ca07
Jonas Lara <adrianlr2515@outlook.com>
2024-01-03 14:37:37 -0600

    lol

--- Problems/Linked-lists/Reorder-List/Reorder-List.cs
+++ Problems/Linked-lists/Reorder-List/Reorder-List.cs
@@ -1,14 +1,15 @@
-﻿/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
+﻿
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution
 {
     private int GetLength(ListNode head)
