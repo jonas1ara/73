fa0746083ec8fb8d14f057ce64298e92e9c71026
Jonas Lara <adrianlr2515@outlook.com>
2024-01-03 20:30:24 -0600

    Merge-Two-Sorted-Lists code completed

--- Problems/Linked-lists/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.cs
+++ Problems/Linked-lists/Merge-Two-Sorted-Lists/Merge-Two-Sorted-Lists.cs
@@ -58,7 +58,7 @@ class Program
         }
         Console.Write("]");
     }
-    public static void Main()
+    static void Main()
     {
         ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
         ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
