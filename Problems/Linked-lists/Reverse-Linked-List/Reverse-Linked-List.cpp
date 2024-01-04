e58e424ee04a90cbbe4de2b584b854091b1d76ae
Jonas Lara <adrianlr2515@outlook.com>
2024-01-03 20:25:09 -0600

    Reverse-Linked-List code completed

--- Problems/Linked-lists/Reverse-Linked-List/Reverse-Linked-List.cpp
+++ Problems/Linked-lists/Reverse-Linked-List/Reverse-Linked-List.cpp
@@ -1,28 +1,37 @@
-﻿/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
+﻿#include <iostream>
+#include <array>
+
+// Using recursion - Time: O(n)
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
 
 class Solution {
-    array<ListNode*, 2> reverse(ListNode *head) {
+    std::array<ListNode*, 2> reverse(ListNode *head) 
+    {
         if (!head->next) return {head, head};
+    
         auto [h, t] = reverse(head->next);
         t->next = head;
         head->next = NULL;
+    
         return {h, head};
     }
+
 public:
-    ListNode* reverseList(ListNode* head) {
+    ListNode* reverseList(ListNode* head) 
+    {
         if (!head) return NULL;
         return reverse(head)[0];
     }
 };
+
+// Using iteration - Time: O(n)
 
 // class Solution {
 // public:
@@ -37,3 +46,39 @@ public:
 //         return h.next;
 //     }
 // };
+
+void printList(ListNode *node)
+{
+    std::cout << "[";
+    while (node != nullptr)
+    {
+        std::cout << node->val;
+        node = node->next;
+        if (node != nullptr)
+        {
+            std::cout << ", ";
+        }
+    }
+    std::cout << "]";
+}
+
+int main()
+{
+    ListNode *list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+    std::cout << "Input: head = ";
+    printList(list);
+    std::cout << std::endl;
+
+    Solution sol;
+    ListNode *reversedList = sol.reverseList(list);
+
+    std::cout << "Output: ";
+    printList(reversedList);
+    std::cout << std::endl;
+
+    delete list;
+
+    return 0;
+}
+
--- Problems/Linked-lists/Reverse-Linked-List/Reverse-Linked-List.cs
+++ Problems/Linked-lists/Reverse-Linked-List/Reverse-Linked-List.cs
@@ -1,22 +1,28 @@
-﻿/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     public int val;
- *     public ListNode next;
- *     public ListNode(int val=0, ListNode next=null) {
- *         this.val = val;
- *         this.next = next;
- *     }
- * }
- */
+﻿using System;
+
+// Using recursion - Time: O(n)
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class Solution
 {
     private (ListNode, ListNode) Reverse(ListNode head)
     {
         if (head.next == null) return (head, head);
+        
         var (h, t) = Reverse(head.next);
         t.next = head;
         head.next = null;
+
         return (h, head);
     }
 
@@ -26,6 +32,8 @@ public class Solution
         return Reverse(head).Item1;
     }
 }
+
+// Using iteration - Time: O(n)
 
 // public class Solution {
 //     public ListNode ReverseList(ListNode head) {
@@ -40,3 +48,35 @@ public class Solution
 //     }
 // }
 
+class Program
+{
+    static void PrintList(ListNode node)
+    {
+        Console.Write("[");
+        while (node != null)
+        {
+            Console.Write(node.val);
+            node = node.next;
+            if (node != null)
+            {
+                Console.Write(", ");
+            }
+        }
+        Console.Write("]");
+    }
+    static void Main(string[] args)
+    {
+        ListNode list = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+
+        Console.Write("Input: head = ");
+        PrintList(list);
+        Console.WriteLine();
+
+        Solution sol = new Solution();
+        ListNode reversedList = sol.ReverseList(list);
+
+        Console.Write("Output: ");
+        PrintList(reversedList);
+        Console.WriteLine();
+    }
+}
