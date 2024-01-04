9458c996722ea57e5cd0b4b477be8337df165f4e
Jonas Lara <adrianlr2515@outlook.com>
2024-01-04 00:46:05 -0600

    House-Robber

--- Problems/Dynamic-programming/House-Robber/House-Robber.cpp
+++ Problems/Dynamic-programming/House-Robber/House-Robber.cpp
@@ -2,14 +2,20 @@
 #include <vector>
 #include <algorithm>
 
+// Using bottom-up approach - Time: O(n)
+
 class Solution {
 public:
-    int rob(std::vector<int>& nums) {
+    int rob(std::vector<int>& nums) 
+    {
         int rob = 0, skip = 0;
-        for (int n : nums) {
+    
+        for (int n : nums) 
+        {
             int r = n + skip, s = std::max(rob, skip);
             rob = r, skip = s;
         }
+        
         return std::max(rob, skip);
     }
 };
@@ -17,8 +23,19 @@ public:
 int main()
 {
     std::vector<int> nums = { 1, 2, 3, 1 };
-    Solution solution;
-    std::cout << solution.rob(nums) << std::endl;
+    std::cout << "Input: nums = [";
+    for (int i = 0; i < nums.size(); i++)
+    {
+        std::cout << nums[i];
+        if (i != nums.size() - 1)
+            std::cout << ", ";
+    }
+    std::cout << "]" << std::endl;
+
+    Solution sol;
+    int result = sol.rob(nums);
     
+    std::cout << "Output: "<< result << std::endl;
+
     return 0;
 }
--- Problems/Dynamic-programming/House-Robber/House-Robber.cs
+++ Problems/Dynamic-programming/House-Robber/House-Robber.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 
+// Using bottom-up approach - Time: O(n)
+
 class Solution
 {
     public int Rob(int[] nums)
     {
         int rob = 0, skip = 0;
+        
         foreach (int n in nums)
         {
             int r = n + skip;
@@ -12,14 +15,21 @@ class Solution
             rob = r;
             skip = s;
         }
+
         return Math.Max(rob, skip);
     }
+}
 
+class Program
+{
     static void Main(string[] args)
     {
-        Solution solution = new Solution();
         int[] nums = { 2, 7, 9, 3, 1 };
-        int result = solution.Rob(nums);
-        Console.WriteLine("Result: " + result);
+        Console.WriteLine("Input: nums = [" + string.Join(", ", nums) + "]");
+
+        Solution sol = new Solution();
+        int result = sol.Rob(nums);
+        
+        Console.WriteLine("Output: " + result);
     }
 }
