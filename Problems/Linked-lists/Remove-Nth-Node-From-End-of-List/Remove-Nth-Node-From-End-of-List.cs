4e9301d9186f9af4d0ad236514de9331fa577876
Jonas Lara <adrianlr2515@outlook.com>
2024-01-05 22:31:42 -0600

    Remove-Nth-Node-From-End-of-List code completed

--- Problems/Linked-lists/Remove-Nth-Node-From-End-of-List/Remove-Nth-Node-From-End-of-List.cs
+++ Problems/Linked-lists/Remove-Nth-Node-From-End-of-List/Remove-Nth-Node-From-End-of-List.cs
@@ -56,6 +56,7 @@ class Program
         }
         Console.Write("]");
     }
+    
     static void Main()
     {
         ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
