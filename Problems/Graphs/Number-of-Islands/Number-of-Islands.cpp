5c42b7f1edb7c76371845dda7e488537980e4d33
Jonas Lara <adrianlr2515@outlook.com>
2024-01-11 15:13:56 -0600

    Number-of-Islands

--- Problems/Graphs/Number-of-Islands/Number-of-Islands.cpp
+++ Problems/Graphs/Number-of-Islands/Number-of-Islands.cpp
@@ -1,23 +1,71 @@
-﻿class Solution {
-    int M, N;
-    int dirs[4][2] = {{0,1},{0,-1},{1,0},{-1,0}};
-    void dfs(vector<vector<char>>& A, int x, int y) {
-        if (x < 0 || y < 0 || x >= M || y >= N || A[x][y] != '1') return;
-        A[x][y] = 'x';
-        for (auto &dir : dirs) dfs(A, x + dir[0], y + dir[1]);
+﻿#include <iostream>
+#include <vector>
+
+class Solution
+{
+    int m, n;
+    int dirs[4][2] = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+    
+    void dfs(std::vector<std::vector<char>> &grid, int x, int y)
+    {
+        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] != '1')
+            return;
+        grid[x][y] = 'x';
+        for (auto &dir : dirs)
+            dfs(grid, x + dir[0], y + dir[1]);
     }
+
 public:
-    int numIslands(vector<vector<char>>& A) {
-        if (A.empty() || A[0].empty()) return 0;
+    int numIslands(std::vector<std::vector<char>> &grid)
+    {
+        if (grid.empty() || grid[0].empty())
+            return 0;
         int ans = 0;
-        M = A.size(), N = A[0].size();
-        for (int i = 0; i < M; ++i) {
-            for (int j = 0; j < N; ++j) {
-                if (A[i][j] != '1') continue;
+        m = grid.size(), n = grid[0].size();
+        for (int i = 0; i < m; ++i)
+        {
+            for (int j = 0; j < n; ++j)
+            {
+                if (grid[i][j] != '1')
+                    continue;
                 ++ans;
-                dfs(A, i, j);
+                dfs(grid, i, j);
             }
         }
+
         return ans;
     }
 };
+
+int main()
+{
+    // Constructing the grid
+    std::vector<std::vector<char>> grid = {
+        {'1', '1', '0', '0', '0'},
+        {'1', '1', '0', '0', '0'},
+        {'0', '0', '1', '0', '0'},
+        {'0', '0', '0', '1', '1'}
+    };
+
+    // Print grid
+    std::cout << "Grid:" << std::endl;
+    for (const auto &row : grid)
+    {
+        for (char cell : row)
+        {
+            std::cout << cell << " ";
+        }
+        std::cout << std::endl;
+    }
+
+    // Create an instance of the Solution class
+    Solution solution;
+
+    // Call the numIslands function
+    int numIslands = solution.numIslands(grid);
+
+    // Print answer
+    std::cout << "Output: " << numIslands << std::endl;
+
+    return 0;
+}
--- Problems/Graphs/Number-of-Islands/Number-of-Islands.cs
+++ Problems/Graphs/Number-of-Islands/Number-of-Islands.cs
@@ -1,17 +1,20 @@
-﻿public class Solution
+﻿using System;
+using System.Collections.Generic;
+
+public class Solution
 {
-    private int M, N;
+    private int m, n;
     private int[][] dirs = { new int[] { 0, 1 }, new int[] { 0, -1 }, new int[] { 1, 0 }, new int[] { -1, 0 } };
 
-    private void DFS(char[][] grid, int x, int y)
+    private void dfs(char[][] grid, int x, int y)
     {
-        if (x < 0 || y < 0 || x >= M || y >= N || grid[x][y] != '1') return;
+        if (x < 0 || y < 0 || x >= m || y >= n || grid[x][y] != '1') return;
 
         grid[x][y] = 'x';
 
         foreach (var dir in dirs)
         {
-            DFS(grid, x + dir[0], y + dir[1]);
+            dfs(grid, x + dir[0], y + dir[1]);
         }
     }
     // grid
@@ -20,19 +23,60 @@
         if (grid == null || grid.Length == 0 || grid[0].Length == 0) return 0;
 
         int ans = 0;
-        M = grid.Length;
-        N = grid[0].Length;
+        m = grid.Length;
+        n = grid[0].Length;
 
-        for (int i = 0; i < M; i++)
+        for (int i = 0; i < m; i++)
         {
-            for (int j = 0; j < N; j++)
+            for (int j = 0; j < n; j++)
             {
                 if (grid[i][j] != '1') continue;
                 ans++;
-                DFS(grid, i, j);
+                dfs(grid, i, j);
             }
         }
 
         return ans;
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Constructing the grid
+        char[][] grid = new char[][]
+        {
+            new char[] {'1', '1', '0', '0', '0'},
+            new char[] {'1', '1', '0', '0', '0'},
+            new char[] {'0', '0', '1', '0', '0'},
+            new char[] {'0', '0', '0', '1', '1'}
+        };
+
+        // Print grid
+        Console.WriteLine("Grid:");
+        PrintGrid(grid);
+
+        // Create an instance of the Solution class
+        Solution solution = new Solution();
+
+        // Call the NumIslands function
+        int numIslands = solution.NumIslands(grid);
+
+        // Print answer
+        Console.WriteLine("Number of Islands: " + numIslands);
+    }
+
+    // Helper method to print the grid
+    static void PrintGrid(char[][] grid)
+    {
+        foreach (var row in grid)
+        {
+            foreach (var cell in row)
+            {
+                Console.Write(cell + " ");
+            }
+            Console.WriteLine();
+        }
     }
 }
