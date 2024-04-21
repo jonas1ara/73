a752cd616b69ebf213658987fde5f673d6e85822
Jonas Lara <adrianlr2515@outlook.com>
2024-04-20 18:02:17 -0600

    Word-Search code completed

--- Problems/Matrix/Word-Search/Word-Search.fsx
+++ Problems/Matrix/Word-Search/Word-Search.fsx
@@ -1,50 +1,53 @@
 open System
 
-let exist (board: char[][]) (word: string) =
-    let m = Array.length board
-    let n = Array.length board.[0]
-    let dirs = [| [|0; 1|]; [|0; -1|]; [|1; 0|]; [|-1; 0|] |]
+type Solution() =
+    member this.Exist(board: char[][], word: string) =
+        let m = Array.length board
+        let n = Array.length board.[0]
+        let dirs = [| [| 0; 1 |]; [| 0; -1 |]; [| 1; 0 |]; [| -1; 0 |] |]
 
-    let rec dfs x y i =
-        if x < 0 || x >= m || y < 0 || y >= n || board.[x].[y] <> word.[i] then false
-        elif i + 1 = word.Length then true
-        else
-            let c = board.[x].[y]
-            board.[x].[y] <- '0' // Marcar como visitado
+        let found = ref false // Variable mutable para indicar si se encontró la palabra
 
-            for dir in dirs do
-                let dx, dy = dir.[0], dir.[1]
-                if dfs (x + dx) (y + dy) (i + 1) then return true
+        let rec dfs x y i =
+            if x < 0 || x >= m || y < 0 || y >= n || board.[x].[y] <> word.[i] then false
+            else if i + 1 = word.Length then
+                found := true
+                true
+            else
+                let c = board.[x].[y]
+                board.[x].[y] <- '0' // Mark as visited
 
-            board.[x].[y] <- c // Restaurar el valor original
-            false
+                for dir in dirs do
+                    let dx, dy = dir.[0], dir.[1]
+                    if not !found && dfs (x + dx) (y + dy) (i + 1) then ()
 
-    for i = 0 to m - 1 do
-        for j = 0 to n - 1 do
-            if dfs i j 0 then return true
+                board.[x].[y] <- c // Restore original value
+                false
 
-    false
+        for i = 0 to m - 1 do
+            for j = 0 to n - 1 do
+                if not !found && dfs i j 0 then ()
 
+        !found
 
-let main argv =
-    let board = [|
-        [| 'A'; 'B'; 'C'; 'E' |]
-        [| 'S'; 'F'; 'C'; 'S' |]
+let board = 
+    [| 
+        [| 'A'; 'B'; 'C'; 'E' |];
+        [| 'S'; 'F'; 'C'; 'S' |];
         [| 'A'; 'D'; 'E'; 'E' |]
     |]
 
-    let word = "ABCCED"
+let word = "ABCCED"
 
-    let result = exist board word
-    if result then
-        printfn "La palabra '%s' se encuentra en el tablero." word
-    else
-        printfn "La palabra '%s' no se encuentra en el tablero." word
+printfn "Input: board = "
+board |> Array.iter (fun row -> printfn "%A" row)
+printfn "\nInput: word = %s\n" word
 
-    0 // Return an exit code
+let sol = Solution()
+let result = sol.Exist(board, word)
 
-[<EntryPoint>]
-let main argv =
-    main argv
-
+if result then
+    printfn "Output: true"
+else
+    printfn "Output: false"
 
